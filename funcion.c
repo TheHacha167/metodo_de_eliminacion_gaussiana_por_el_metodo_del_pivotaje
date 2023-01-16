badb6e65aefbecdbcc6d423e37751acefd73818a
Jesus_hna <thehacha167@gmail.com>
2023-01-16 17:58:40 +0100

    .

--- funcion.c
+++ funcion.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-void imprimirMatriz(double **A, int n) {
+// Función que hace la salida de la matriz final
+void imprimirMatriz(double **A, int n)
+{
     int i, j;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
             printf("%.2lf ", A[i][j]);
         }
         printf("\n");
     }
 }
+/*
+La función main pide al usuario el tamaño
+de la matriz cuadrada (este paso se puede omitir por una variable global fija)
+y asignando memoria dinámica para esta matriz y un arreglo llamado "scale".
+Luego se pide al usuario que ingrese los valores de la matriz.
+
+El siguiente bucle for calcula el valor máximo de cada fila de la
+matriz A y lo almacena.
+
+Hay un bucle for anidado que se encarga de encontrar el elemento
+mayor de la columna y cambia su correspondiente fila con k ( la fila k es solo si no está previamente ),
+
+El siguiente for es para convertir la matriz A en la matriz.
+
+Usamos el "free(scale);" para liberar la memoria de la variable
+y despúes hacemos la llamada a la funcion que imprime la matriz A.
+*/
 int main()
 {
     int n, i, j, k, maxI, temp;
     double maxVal, tempf, **A;
     double *scale;
-
 
     printf("Ingrese el tamaño de la matriz cuadrada: ");
     scanf("%d", &n);
@@ -82,9 +101,9 @@ int main()
             }
         }
     }
-    
+
     free(scale);
-    imprimirMatriz(A,n);
-    
+    imprimirMatriz(A, n);
+
     return 0;
 }
